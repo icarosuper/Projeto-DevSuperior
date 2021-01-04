0be30d91dd370e7ee44abc7542d702eda1cc3e2e
Ícaro Motta <icarosuperskate@gmail.com>
2021-01-04 13:41:34 -0300

    Domain model

--- dsdeliver/back-end/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ dsdeliver/back-end/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -5,19 +5,39 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Order implements Serializable{
-	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String adress;
+	private String address;
 	private Double latitude;
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+
+	@ManyToMany
+	@JoinTable(name = "tb_order_product", 
+		joinColumns = @JoinColumn(name = "order_id"), 
+		inverseJoinColumns = @JoinColumn(name = "product_id"))
 	
 	private Set<Product> products = new HashSet<>();
-	
-	public Order() {}
+
+	public Order() {
+	}
 
 	public Order(Long id, String adress, Double latitude, Double longitude, Instant moment, OrderStatus status) {
 		super();
--- dsdeliver/back-end/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
+++ dsdeliver/back-end/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -2,16 +2,28 @@ package com.devsuperior.dsdeliver.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
 public class Product implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double price;
 	private String description;
 	private String imageUri;
-	
-	public Product() {}
+
+	public Product() {
+	}
 
 	public Product(Long id, String name, Double price, String description, String imageUri) {
 		super();
@@ -86,6 +98,5 @@ public class Product implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
