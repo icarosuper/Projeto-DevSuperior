650336ebcfe5d18c0eefc1aca3d9e8fecd2a02d1
Ícaro Motta <icarosuperskate@gmail.com>
2021-01-10 12:33:40 -0300

    Mapa e Search order location

--- dsdeliver/front-web/src/api.ts
+++ dsdeliver/front-web/src/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const url = 'https://icaro-motta-dsdelivery.herokuapp.com';
+const mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 export function fetchProducts(){
 	return axios(`${url}/products`);
+}
+
+export const fetchLocalMapBox = async (local: string) => {
+	return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
 }
